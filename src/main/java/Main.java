a7b594efc8bd4c31ae906e8c27b1c320d982700d
sabby155 <nebula155@gmail.com>
2022-06-23 15:24:48 -0400

    Scope Lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         // your code here
+        boolean flag = false;
+        int number;
+        if (flag) {
+            // do something when flag is true
+            number = 12;
+        } else {
+            // do something else when flag is false
+            number = 5;
+        }
+        System.out.println(number);
     }
 }
